4d38272c56af6a97a093211ee0e940123908417b
EvansChang <85141291@qq.com>
2023-09-05 17:22:45 +0800

    add variable and compound

---
+++ compound/src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    let months = ["January","Fabruary","March","April","May","June","July","August","September","October","November","December"];
+
+    let first = months[0];
+    let second = months[1];
+
+    println!("first month is {},second month is {}",first,second);
+    
+}
---
+++ variable/src/main.rs
@@ -0,0 +1,18 @@
+const MAX_POINTS:u32=100_000;
+
+fn main() {
+    println!("Hello, world!");
+    let mut x = 5;
+    println!("The value of x is {}",x);
+    x= 6;
+    println!("The value of x is {}",x);
+
+    let y =5;
+    let y=y+1;
+    let y=y*2;
+    println!("The value of y is {}",y);
+
+    let spaces ="       ";
+    let spaces = spaces.len();
+    println!("{}",spaces);
+}
