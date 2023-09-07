6f27f246c379bf19b8c833b9be3e6dcf6141dc1b
EvansChang <85141291@qq.com>
2023-09-07 14:51:21 +0800

    branches example

--- branches/src/main.rs
+++ branches/src/main.rs
@@ -15,4 +15,26 @@ fn main() {
     let number = if condition{5}else{6};
     println!("The value of number is : {}",number);
 
+    let mut number = 3;
+    while number !=0 {
+        println!("{}",number);
+        number -=1;
+    }
+    println!("LIFTOFF!");
+
+    let a = [10,20,30,40,50];
+    let mut index = 0;
+    while index < 5 {
+        println!("the value is : {}",a[index]);
+        index +=1;
+    }
+
+    for element in a.iter(){
+        println!("the value is : {}",element);
+    }
+
+    for number in (1..4).rev(){
+        println!("{}!",number)
+    }
+    println!("LIFTOFF!");
 }
