ab05306659a6448d3f511d78400b10b600a2d4ab
EvansChang <85141291@qq.com>
2023-09-07 14:20:20 +0800

    add compound branches example

--- compound/src/main.rs
+++ compound/src/main.rs
@@ -5,5 +5,16 @@ fn main() {
     let second = months[1];
 
     println!("first month is {},second month is {}",first,second);
-    
+
+    let x = plus_five(6);
+
+    let y = {
+        let x = 1;
+        x+3
+    };
+    println!("x is {} and y is {}",x,y);
+}
+
+fn plus_five(x: i32)->i32{
+    x+5
 }
